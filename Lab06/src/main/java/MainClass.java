7c6fdb316f0baf26e081cf67aeb5a8003c247dfe
Saba Jamalian <jamalian.saba@gmail.com>
2021-02-28 01:07:57 -0600

    fixes for minor syntax issues as well as some implementation details with the draw unfilled algorithm

--- Lab06/src/main/java/MainClass.java
+++ Lab06/src/main/java/MainClass.java
@@ -1,22 +1,11 @@
 public class MainClass {
     
     public static void main(String[] args){
-        Rectangle r1 = new Rectangle(5, 8);
-        if (Rectangle.filled = false){ //how do I properly call the "filled" from the Rectangle class?//
-            r1.draw();
-        }
-        else {
-            r1.drawUnfilled();
-                    }
+        Rectangle r1 = new Rectangle(5, 8, true);
+        r1.draw();
         
-        Rectangle r2 = new Rectangle(8, 5);
-        if (Rectangle.filled = false){
-            r2.draw();
-        }
-        else{
-            r2.drawUnfilled();
-        }
-        //this is another advantage of constructors is that you can use the same attributes for different method inputs
+        Rectangle r2 = new Rectangle(3, 4, false);
+        r2.draw();
     }
     
 }
--- Lab06/src/main/java/Rectangle.java
+++ Lab06/src/main/java/Rectangle.java
@@ -1,37 +1,53 @@
+
 public class Rectangle {
-    
+
     int width;
     int height;
     boolean filled = false;
 //contructors are useful for not having to define all attributed
 //everytime you create a new class
-    public Rectangle(int width, int height) {
+
+    public Rectangle(int width, int height, Boolean filled) {
         this.width = width;
         this.height = height;
+        this.filled = filled;
     }
-    
+
     public void draw(){
-            for (int i = 0; i < height; i++){
-            for (int j = 0; j < width; j++){
-                System.out.print("*"); //print and println are different, println will go to next line, print will keep going on the same line
+        if(filled){
+            drawfilled();
         }
-            System.out.println("");
+        else{
+            drawUnfilled();
         }
-        }
-    
-    public void drawUnfilled(){
-        for (int i = 0; i <= height; i++){
-            for (int j = 0; j <= width; j++){
-                if ((i == 0 || height) || (j == 0 || width)){
-                System.out.print("*"); //print and println are different, println will go to next line, print will keep going on the same line
-        }
-            System.out.println("");
-        }
-        }
-        
     }
-    
+    public void drawfilled() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print("*"); //print and println are different, println will go to next line, print will keep going on the same line
+            }
+            System.out.println("");
+        }
+    }
+
+    public void drawUnfilled() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((j == width - 1)){
+                    System.out.println("*");
+                }
+                else if ((i == 0) || (j == 0) || (i == height - 1)) {
+                    System.out.print("*"); //print and println are different, println will go to next line, print will keep going on the same line
+                }
+                else{
+                    System.out.print(" ");
+                }
+                
+            }
+        }
+
+    }
+
 }
-    //end of draw method
-    
+//end of draw method
 
